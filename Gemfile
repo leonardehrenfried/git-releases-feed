f5b24df7b7f761a495f0075a2a6f1eb7b7065b88
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-03-06 17:59:12 +0100

    Use RDiscount for Markdown parsing

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
 gem "hpricot", "~> 0.8.6"
-gem "redcarpet", "~> 2.1.0"
+gem "rdiscount", "~> 1.6.8"
--- generate_feed.rb
+++ generate_feed.rb
@@ -3,9 +3,19 @@
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
+require 'rss/maker'
+require 'rdiscount'
+
+
 
 doc = Hpricot(open("http://git-scm.com/"))
 ver = doc.at("div#ver").inner_text
 ver = ver[1..-1] #remove leading 'v'
 
-print ver
+url = "https://raw.github.com/gitster/git/master/Documentation/RelNotes/#{ver}.txt" 
+release_notes = open(url){ |f| f.read }
+markdown = RDiscount.new(release_notes)
+html_notes = markdown.to_html
+
+puts html_notes
+
