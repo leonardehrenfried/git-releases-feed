065f52a400d9129c41ac60ed6fef83ca9302bb28
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-03-06 18:12:38 +0100

    Create basic RSS feed

--- generate_feed.rb
+++ generate_feed.rb
@@ -6,8 +6,6 @@ require 'open-uri'
 require 'rss/maker'
 require 'rdiscount'
 
-
-
 doc = Hpricot(open("http://git-scm.com/"))
 ver = doc.at("div#ver").inner_text
 ver = ver[1..-1] #remove leading 'v'
@@ -18,4 +16,23 @@ markdown = RDiscount.new(release_notes)
 html_notes = markdown.to_html
 
 puts html_notes
+
+version = "2.0"
+destination = "feed.xml"
+
+feed = RSS::Maker.make(version) do |m|
+  m.channel.title = "Git Release Notes"
+  m.channel.link = "http://lenni.info"
+  m.channel.description = "Git Release notes."
+  m.items.do_sort = true
+
+  i = m.items.new_item
+  i.title = "Git #{ver} release notes" 
+  i.link = url
+  i.time = Time.now
+end
+
+File.open(destination, "w") do |f|
+  f.write(feed)
+end
 
