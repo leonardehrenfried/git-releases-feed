d7e239f49f26bd07aa48bf69f5405da5858c0d40
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-03-07 11:07:30 +0100

    Refactoring into functions

--- generate_feed.rb
+++ generate_feed.rb
@@ -6,30 +6,47 @@ require 'open-uri'
 require 'rss/maker'
 require 'rdiscount'
 
+
+FILENAME = "rss.xml"
+
+def is_new(version)
+  puts value 
+end
+
+def get_url(version)
+  "https://raw.github.com/gitster/git/master/Documentation/RelNotes/#{version}.txt"
+end
+
+def add_new(version)
+  release_notes = open(get_url version){ |f| f.read }
+  markdown = RDiscount.new(release_notes)
+  html_notes = markdown.to_html
+  
+  feed = RSS::Maker.make("2.0") do |m|
+    m.channel.title = "Git Release Notes"
+    m.channel.link = "http://git-com.com"
+    m.channel.description = "Git Release notes."
+    m.items.do_sort = true
+
+    i = m.items.new_item
+    i.title = "Git #{version} Release Notes" 
+    i.link = get_url version
+    i.description = html_notes
+    i.date = Time.now
+  end
+
+  write_to_file feed
+end
+
+def write_to_file(feed)
+  File.open(FILENAME, "w") do |f|
+    f.write(feed)
+  end
+end
+
 doc = Hpricot(open("http://git-scm.com/"))
 ver = doc.at("div#ver").inner_text
 ver = ver[1..-1] #remove leading 'v'
+add_new ver
 
-url = "https://raw.github.com/gitster/git/master/Documentation/RelNotes/#{ver}.txt" 
-release_notes = open(url){ |f| f.read }
-markdown = RDiscount.new(release_notes)
-html_notes = markdown.to_html
-
-feed = RSS::Maker.make("2.0") do |m|
-  m.channel.title = "Git Release Notes"
-  m.channel.link = "http://git-com.com"
-  m.channel.description = "Git Release notes."
-  m.items.do_sort = true
-
-  i = m.items.new_item
-  i.title = "Git #{ver} release notes" 
-  i.link = url
-  i.description = html_notes
-  i.date = Time.now
-end
-
-destination = "rss.xml"
-File.open(destination, "w") do |f|
-  f.write(feed)
-end
 
