9b0a35bf81f21c01c61dafd2fbc6fa2f0bdaaea3
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2013-06-09 18:03:55 +0000

    Limit feed to the 15 newest items

--- generate_feed.rb
+++ generate_feed.rb
@@ -46,6 +46,7 @@ def add_item(version)
   item.description = html_notes
   item.guid = RSS::Rss::Channel::Item::Guid.new
 
+  feed.channel.items = feed.channel.items[0..15]
   feed.channel.items.unshift item
   write_to_file feed
 end
